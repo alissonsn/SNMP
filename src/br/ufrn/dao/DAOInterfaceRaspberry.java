be67d2a79d2d1c8273f1bf470ebf6fb5d8ecdfd9
Silas Tiago de Medeiros <silas@localhost.localdomain>
2016-08-16 22:55:16 -0300

    teste de listagem do raspberry

--- src/br/ufrn/dao/DAOInterfaceRaspberry.java
+++ src/br/ufrn/dao/DAOInterfaceRaspberry.java
@@ -15,17 +15,20 @@ public class DAOInterfaceRaspberry {
 	public DAOInterfaceRaspberry(){
 		this.conexao = new ConnectionFactory().getConnection();
 	}
-	
-	
 
 	public void adicionarInterfaceRaspbery(Interface_Raspberry interface_rapsberry){
 		String sql = "INSERT INTO interface_raspberry (interface, id_raspberry, id_switch) VALUES (?,?,?)";
 		System.out.println("SQL: " + sql);
+		
+		System.out.println("Interface: " + interface_rapsberry.getInterface_raspberry());
+		System.out.println("Id do raspberry: " +interface_rapsberry.getRaspberry().getId_raspberry());
+		System.out.println("Id do Switch: " +interface_rapsberry.getComutador().getId_switch());
+		
 		try{
 			PreparedStatement ps = conexao.prepareStatement(sql);
 			ps.setString(1, interface_rapsberry.getInterface_raspberry());
 			ps.setLong(2, interface_rapsberry.getRaspberry().getId_raspberry());
-			ps.setLong(3, interface_rapsberry.getRaspberry().getComutador().getId_switch());
+			ps.setLong(3, interface_rapsberry.getComutador().getId_switch());
 			ps.execute();
 			ps.close();
 		}catch(SQLException erro){
--- src/br/ufrn/model/Interface_Raspberry.java
+++ src/br/ufrn/model/Interface_Raspberry.java
@@ -8,14 +8,22 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
 
+
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement
 public class Interface_Raspberry implements Serializable{
 
 	
 	private String interface_raspberry;
-	private Raspberry raspberry = null;
-	private Switch comutador = null;
+	private Raspberry raspberry = new Raspberry();
+	private Switch comutador = new Switch();
+	private Municipio municipio = new Municipio();
+	private Unidade unidade = new Unidade();
+	private Predio predio = new Predio();
+	private Pavimento pavimento = new Pavimento();
+	private Andar andar = new Andar();
+	private Sala sala = new Sala();
+	private Rack rack = new Rack();
 	
 	public String getInterface_raspberry() {
 		return interface_raspberry;
@@ -35,4 +43,46 @@ public class Interface_Raspberry implements Serializable{
 	public void setComutador(Switch comutador) {
 		this.comutador = comutador;
 	}
+	public Municipio getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(Municipio municipio) {
+		this.municipio = municipio;
+	}
+	public Unidade getUnidade() {
+		return unidade;
+	}
+	public void setUnidade(Unidade unidade) {
+		this.unidade = unidade;
+	}
+	public Predio getPredio() {
+		return predio;
+	}
+	public void setPredio(Predio predio) {
+		this.predio = predio;
+	}
+	public Pavimento getPavimento() {
+		return pavimento;
+	}
+	public void setPavimento(Pavimento pavimento) {
+		this.pavimento = pavimento;
+	}
+	public Andar getAndar() {
+		return andar;
+	}
+	public void setAndar(Andar andar) {
+		this.andar = andar;
+	}
+	public Sala getSala() {
+		return sala;
+	}
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	public Rack getRack() {
+		return rack;
+	}
+	public void setRack(Rack rack) {
+		this.rack = rack;
+	}	
 }
